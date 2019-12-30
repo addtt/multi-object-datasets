b2ac0235ad2e2722de23649edf3b8168fe0f3d01
Andrea Dittadi <andrea.dittadi@gmail.com>
2019-12-30 01:04:48 +0100

    Refactoring

--- generate_dataset.py
+++ generate_dataset.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from multiobject import generate_multiobject_dataset
 from multiobject.sprites import generate_dsprites, generate_binary_mnist
-from utils import get_date_str
+from utils import get_date_str, show_img_grid
 
 supported_sprites = ['dsprites', 'binary_mnist']
 
@@ -70,29 +70,6 @@ def main():
     plt.hist(n_obj, np.arange(min(n_obj) - 0.5, max(n_obj) + 0.5 + 1, 1))
     plt.title("Distribution of num objects per image")
     plt.xlabel("Number of objects")
-    plt.show()
-
-
-def show_img_grid(n, imgs, labels=None):
-    plt.figure(figsize=(n * 2, n * 2))
-    for i in range(n ** 2):
-        plt.subplot(n, n, i + 1)
-        plt.imshow(imgs[i].squeeze(), vmin=0, vmax=255)
-        plt.gca().tick_params(
-            axis='both',
-            which='both',  # both major and minor ticks are affected
-            left=False,  # ticks are off on all edges
-            right=False,
-            bottom=False,
-            top=False,
-            labelleft=False,  # labels are off on all edges
-            labelbottom=False)
-        plt.subplots_adjust(top=.99, bottom=.01,
-                            left=.01, right=.99,
-                            wspace=.01, hspace=.01)
-        if labels is not None:
-            lst = ["{} {}".format(k, labels[k][i]) for k in labels.keys()]
-            print(list(lst))
     plt.show()
 
 
--- utils/misc.py
+++ utils/misc.py
@@ -1,4 +1,29 @@
 import datetime
 
+import matplotlib.pyplot as plt
+
+
 def get_date_str():
     return datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+
+def show_img_grid(n, imgs, labels=None):
+    plt.figure(figsize=(n * 2, n * 2))
+    for i in range(n ** 2):
+        plt.subplot(n, n, i + 1)
+        plt.imshow(imgs[i].squeeze(), vmin=0, vmax=255)
+        plt.gca().tick_params(
+            axis='both',
+            which='both',  # both major and minor ticks are affected
+            left=False,  # ticks are off on all edges
+            right=False,
+            bottom=False,
+            top=False,
+            labelleft=False,  # labels are off on all edges
+            labelbottom=False)
+        plt.subplots_adjust(top=.99, bottom=.01,
+                            left=.01, right=.99,
+                            wspace=.01, hspace=.01)
+        if labels is not None:
+            lst = ["{} {}".format(k, labels[k][i]) for k in labels.keys()]
+            print(list(lst))
+    plt.show()
